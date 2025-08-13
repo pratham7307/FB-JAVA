60f983404ec5184dedb0e2d32eb5b833a92cfaea
pratham7307 <raipratham8957@gmail.com>
2025-08-13 14:33:25 +0530

    Add files via upload

---
+++ removeduplicates.java
@@ -0,0 +1,15 @@
+package string;
+
+import java.util.Scanner;
+
+public class removeduplicates {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the string");
+        String abcd=obj.nextLine();
+        String result=removeduplicate(abcd);
+        System.out.println("the removed duplicates is:"+result);
+        obj.close();
+    }
+    public static string remove duplicates(string str)
+}
