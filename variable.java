1da32d49a3dabc2ad13b1e956dbf406e16082ebd
pratham7307 <raipratham8957@gmail.com>
2025-08-13 14:31:15 +0530

    java first commit

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        int a=10;
+        float b=19.54f;
+        char c='A';
+        String d="surya";
+        System.out.println(a);
+         System.out.println(b);
+          System.out.println(c);
+         System.out.println(d);
+    }
+}
